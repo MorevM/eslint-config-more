aed36c324e412d373146f6838da949b15b682f12
MorevM <max.seainside@gmail.com>
2022-01-31 18:14:56 +0300

    fix(base): Rule `unicorn/prefer-string-replace-all` turned off for a while

--- configurations/base/plugins/unicorn.js
+++ configurations/base/plugins/unicorn.js
@@ -328,7 +328,8 @@ module.exports = {
 		// Prefer `String#replaceAll()` over regex searches with the global flag (autofixable)
 		// https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-string-replace-all.md
 		// Fails on Node 14
-		'!unicorn/prefer-string-replace-all': 'warn',
+		// TODO: [2023-01-01] Review it again
+		'!unicorn/prefer-string-replace-all': 'off',
 
 		// Prefer `String#slice()` over `String#substr()` and `String#substring()` (partly autofixable)
 		// https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-string-slice.md
