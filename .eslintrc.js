ada0d1503c516b2f5ae1b031e8bb2f34f222b7b9
MorevM <max.seainside@gmail.com>
2022-03-22 10:55:22 +0300

    chore: Repo local `.eslintrc` extends builded config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,19 @@
-const { makeConfig } = require('./utils/tools.js');
-
-const config = makeConfig([
-	{ name: 'base' },
-	{ name: 'browser' },
-	{ name: 'node' },
-	{
-		name: 'vue/vue2',
-		overrides: {
-			files: ['*.vue'],
-		},
-	},
-	{
-		name: 'vue/vue3',
-		overrides: {
-			 // There is no such extension, just needed to test all things working correct
-			files: ['*.vue3'],
-		},
-	},
-	{
-		name: 'jest',
-		overrides: {
-			files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-		},
-	},
-	{
-		name: 'json',
-		overrides: {
-			files: ['*.json', '*.json5', '*.jsonc'],
-		},
-	},
-	{
-		name: 'json/jsonc',
-		overrides: { files: ['*.jsonc', '*.json5', 'jsconfig.json', 'tsconfig.json'] },
-	},
-	{
-		name: 'json/package-json',
-		overrides: {
-			files: ['package.json'],
-		},
-	},
-	{
-		name: 'yaml',
-		overrides: {
-			files: ['*.yaml', '*.yml'],
-		},
-	},
-	{
-		name: 'typescript',
-		overrides: {
-			files: ['*.ts'],
-		},
-	},
-	{
-		name: 'html',
-		overrides: {
-			files: ['*.html'],
-		},
-	},
-	{
-		name: 'markdown',
-		overrides: {
+module.exports = {
+	extends: ['./build/presets/common.js'],
+	overrides: [
+		{
+			files: ['*.js'],
+			rules: {
+				'node/global-require': 'off',
+				'import/no-dynamic-require': 'off',
+			},
+		}, {
 			files: ['*.md'],
+			rules: {
+				// Doesn't respect HTML element indentation
+				'markdownlint/md007': 'off',
+				'no-autofix/markdownlint/md007': 'off',
+			},
 		},
-	},
-].reverse());
-
-config.overrides = config.overrides || [];
-config.overrides.push({
-	files: ['*.js'],
-	rules: {
-		'node/global-require': 'off',
-		'import/no-dynamic-require': 'off',
-	},
-}, {
-	files: ['*.md'],
-	rules: {
-		// Doesn't respect HTML element indentation
-		'markdownlint/md007': 'off',
-		'no-autofix/markdownlint/md007': 'off',
-	},
-});
-
-module.exports = config;
+	],
+};
