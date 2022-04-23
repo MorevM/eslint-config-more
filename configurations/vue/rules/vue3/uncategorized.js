535607ff602acc22fd53ca6dbf657540b18a015f
MorevM <max.seainside@gmail.com>
2022-04-23 14:24:40 +0300

    feat(vue3): Add `vue/define-macros-order`
    Introduced in v8.7.0

--- configurations/vue/rules/vue3/uncategorized.js
+++ configurations/vue/rules/vue3/uncategorized.js
@@ -1,5 +1,11 @@
 module.exports = {
 	rules: {
+		// Enforce order of `defineEmits` and `defineProps` compiler macros (autofixable)
+		// https://eslint.vuejs.org/rules/define-macros-order.html
+		'+vue/define-macros-order': ['warn', {
+			order: ['defineProps', 'defineEmits'],
+		}],
+
 		// Require declare public properties using `expose`
 		// https://eslint.vuejs.org/rules/require-expose.html
 		'vue/require-expose': 'error',
