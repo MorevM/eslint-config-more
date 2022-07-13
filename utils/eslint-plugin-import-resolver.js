24d93e274bd67a03fe9f4cd766509ef890f4bb5d
MorevM <max.seainside@gmail.com>
2022-07-13 19:06:35 +0300

    fix: Correctly resolve full paths in custom resolver

--- utils/eslint-plugin-import-resolver.js
+++ utils/eslint-plugin-import-resolver.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const { builtinModules } = require('module');
 const { resolve: resolveExports } = require('resolve.exports');
 
@@ -12,12 +13,23 @@ const resolve = (source, file, _config) => {
 		const moduleId = require.resolve(source, { paths: [path.dirname(file)] });
 		return { found: true, path: moduleId };
 	} catch (err) {
-		if (err.code === 'MODULE_NOT_FOUND' && err.path?.endsWith('/package.json')) {
+		if (err.code === 'MODULE_NOT_FOUND' && err.path?.endsWith('package.json')) {
 			const { name, module, main, exports } = require(err.path);
 			const resolved = resolveExports({ name, module, main, exports }, source);
 			const moduleId = path.join(path.dirname(err.path), resolved);
 
 			return { found: true, path: moduleId };
+		}
+
+		if (err.code === 'ERR_PACKAGE_PATH_NOT_EXPORTED') {
+			// TODO: Is there a better way?
+			const [, subpath, base] = err.message.match(/subpath ["'](.*)["'] .* in (.*)/) || [null, null, null];
+			if (!base) return { found: false };
+
+			const fullPath = path.join(path.dirname(base), subpath);
+			if (fs.existsSync(fullPath)) {
+				return { found: true, path: fullPath };
+			}
 		}
 
 		return { found: false };
