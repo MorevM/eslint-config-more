097b8903e104e2195a0d3d9dccf5e9b03047318b
MorevM <max.seainside@gmail.com>
2023-12-02 22:35:49 +0300

    feat(vue2)!: Add `vue/v-if-else-key` rule
    BREAKING CHANGE: With new rule `vue/v-if-else-key` all configurations and presets related to `vue2` become more strict.

---
+++ configurations/vue/rules/vue2/uncategorized.js
@@ -0,0 +1,7 @@
+module.exports = {
+	rules: {
+		// Require key attribute for conditionally rendered repeated components (autofixable)
+		// https://eslint.vuejs.org/rules/v-if-else-key.html
+		'vue/v-if-else-key': 'error',
+	},
+};
--- configurations/vue/vue2.js
+++ configurations/vue/vue2.js
@@ -4,6 +4,7 @@ const { base, parts: commonParts } = require('./_settings.js');
 const vue2Parts = [
 	'./rules/vue2/essential.js',
 	'./rules/vue2/strongly-recommended.js',
+	'./rules/vue2/uncategorized.js',
 ].map(part => require(part));
 
 const parts = [...commonParts, ...vue2Parts];
