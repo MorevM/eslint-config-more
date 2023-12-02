8145a662a40b9f6816c9b3f9645d9118c67aebcc
MorevM <max.seainside@gmail.com>
2023-12-02 23:23:48 +0300

    feat(html): Take account of `@html-eslint/require-open-graph-protocol` rule

--- configurations/html/rules/seo.js
+++ configurations/html/rules/seo.js
@@ -15,5 +15,9 @@ module.exports = {
 		// Enforce to use `<meta name="description" ...>` in the `<head></head>`
 		// https://yeonjuan.github.io/html-eslint/docs/rules/require-meta-description
 		'@html-eslint/require-meta-description': 'warn',
+
+		// Enforce to use specified meta tags for open graph protocol
+		// https://html-eslint.org/docs/rules/require-open-graph-protocol
+		'@html-eslint/require-open-graph-protocol': 'off',
 	},
 };
