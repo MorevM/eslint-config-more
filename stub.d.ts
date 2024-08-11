6fc5042811f5968996744e34fac466f36726a1e4
MorevM <max.seainside@gmail.com>
2024-08-11 13:24:38 +0300

    chore: Add stubs for plugins without types

---
+++ stub.d.ts
@@ -0,0 +1,27 @@
+declare module 'eslint-plugin-no-autofix' {
+	import type { ESLint } from 'eslint';
+
+	declare const _default: ESLint.Plugin;
+	export default _default;
+}
+
+declare module 'eslint-plugin-eslint-comments' {
+	import type { ESLint } from 'eslint';
+
+	declare const _default: ESLint.Plugin;
+	export default _default;
+}
+
+declare module 'eslint-plugin-import' {
+	import type { ESLint } from 'eslint';
+
+	declare const _default: ESLint.Plugin;
+	export default _default;
+}
+
+declare module 'eslint-plugin-no-secrets' {
+	import type { ESLint } from 'eslint';
+
+	declare const _default: ESLint.Plugin;
+	export default _default;
+}
