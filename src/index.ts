23270b9cd20f6d6a6eac9bb52c9bce8f676794ea
MorevM <max.seainside@gmail.com>
2024-10-21 15:24:58 +0300

    feat: Add JSDoc's for exported functions

--- src/index.ts
+++ src/index.ts
@@ -39,10 +39,28 @@ type ConfigurationsMap = {
 type Configuration = keyof ConfigurationsMap;
 type ConfigurationOptions<T extends Configuration> = Parameters<ConfigurationsMap[T]>[0];
 
+/**
+ * High-order function for flattening the configurations.
+ * Configurations can consist of several parts,
+ * but the endpoint of an ESLint-config should be a flat array.
+ *
+ * @param   configurations   FlatConfig-compatible configuration or array of such configurations.
+ *
+ * @returns                  Flat list of configurations.
+ */
 export const combine = (...configurations: FlatConfig[]) => {
 	return configurations.flat(Infinity);
 };
 
+/**
+ * A function that enables rules for the passed configuration name.
+ * Each configuration has its own options.
+ *
+ * @param   name      Configuration name.
+ * @param   options   Configuration options.
+ *
+ * @returns           An array of configuration parts to use them as `combine` function arguments.
+ */
 export const defineConfiguration = <T extends Configuration>(name: T, options: ConfigurationOptions<T> = {}) => {
 	switch (name) {
 		case 'javascript': return configurationJavascript(options);
@@ -63,6 +81,18 @@ export const defineConfiguration = <T extends Configuration>(name: T, options: C
 	}
 };
 
+/**
+ * A function that disables all rules for some files.
+ * By default inherits all patterns from `.gitignore` as well as popular artifacts destinations.
+ * This list can be extended via `extraIgnoredGlobs` argument.
+ *
+ * @see https://globster.xyz/
+ *
+ * @param   extraIgnoredGlobs   Extra files to ignore by all rules. \
+ *                              Expected [minimatch](https://github.com/isaacs/minimatch)-compatible array of strings.
+ *
+ * @returns                     FlatConfig-compatible configuration.
+ */
 export const defineIgnores = (extraIgnoredGlobs: string[] = []) => {
 	return {
 		name: 'morev/ignores',
