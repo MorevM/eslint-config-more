c3822d58d4a83799451bced63d2948e0b92c6366
MorevM <max.seainside@gmail.com>
2024-10-22 19:17:19 +0300

    refactor: Use ESLint's types for `RuleValue`

--- src/types/flat-config.ts
+++ src/types/flat-config.ts
@@ -1,8 +1,10 @@
 import type { Linter } from 'eslint';
 
-type RuleLevel = 0 | 1 | 2 | 'off' | 'warn' | 'error';
-
 export type FlatConfig = Linter.Config;
 
-// Actually `undefined` is not valid, but presented in some configuration types ¯\_(ツ)_/¯
-export type RuleValue = RuleLevel | [RuleLevel, any] | [RuleLevel, ...unknown[]] | undefined;
+export type RuleValue = Linter.RuleSeverity
+	| [Linter.RuleSeverity, any]
+	| [Linter.RuleSeverity, ...unknown[]]
+	// Actually `undefined` is not valid,
+	// but presented in some configuration types ¯\_(ツ)_/¯
+	| undefined;
