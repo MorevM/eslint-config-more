263b4072f5f465939955262c2d634b00714bfbc1
MorevM <max.seainside@gmail.com>
2024-10-22 19:13:13 +0300

    refactor: Use ESLint's types for FlatConfig

--- src/types/flat-config.ts
+++ src/types/flat-config.ts
@@ -1,9 +1,8 @@
-import type { ElementOf } from '@morev/utils';
-import type { Config } from 'typescript-eslint';
+import type { Linter } from 'eslint';
 
 type RuleLevel = 0 | 1 | 2 | 'off' | 'warn' | 'error';
 
-export type FlatConfig = ElementOf<Awaited<Config>>;
+export type FlatConfig = Linter.Config;
 
 // Actually `undefined` is not valid, but presented in some configuration types ¯\_(ツ)_/¯
 export type RuleValue = RuleLevel | [RuleLevel, any] | [RuleLevel, ...unknown[]] | undefined;
