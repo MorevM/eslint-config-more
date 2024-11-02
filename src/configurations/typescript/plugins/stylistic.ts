1b812b2f75d448e2e6f9611db7594fb3ce3361d9
MorevM <max.seainside@gmail.com>
2024-11-02 22:42:43 +0300

    feat(typescript): Add `@stylistic/type-generic-spacing` and `@stylistic/type-named-tuple-spacing` rules

--- src/configurations/typescript/plugins/stylistic.ts
+++ src/configurations/typescript/plugins/stylistic.ts
@@ -23,5 +23,13 @@ export default defineConfigurationPart({
 		// Require consistent spacing around type annotations (autofixable)
 		// https://eslint.style/rules/default/type-annotation-spacing
 		'@stylistic/type-annotation-spacing': 'warn',
+
+		// Enforces consistent spacing inside TypeScript type generics (autofixable)
+		// https://eslint.style/rules/default/type-generic-spacing
+		'@stylistic/type-generic-spacing': 'warn',
+
+		// Expect space before the type declaration in the named tuple (autofixable)
+		// https://eslint.style/rules/default/type-named-tuple-spacing
+		'@stylistic/type-named-tuple-spacing': 'warn',
 	},
 });
