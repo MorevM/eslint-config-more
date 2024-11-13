6372ef48fa324205fe28c04ac29148befd750791
MorevM <max.seainside@gmail.com>
2024-11-13 17:57:37 +0300

    chore: Finalize own `eslint.config.json`

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,7 @@ import { combine, defineConfiguration, defineIgnores } from './dist/index.js';
 
 export default combine([
 	defineIgnores({
-		extraIgnoredGlobs: [
-			'bin',
-		],
+		extraIgnoredGlobs: ['bin'],
 	}),
 	defineConfiguration('javascript', {
 		overrides: {
@@ -12,19 +10,34 @@ export default combine([
 			'sonarjs/no-duplicate-string': 'off',
 		},
 	}),
+	defineConfiguration('browser'),
 	defineConfiguration('node'),
-	defineConfiguration('typescript'),
-	defineConfiguration('yaml'),
-	defineConfiguration('markdown'),
-	defineConfiguration('json'),
-	defineConfiguration('vitest'),
 	defineConfiguration('jsx'),
-	// {
-	// 	files: ['*.md'],
-	// 	rules: {
-	// 		// Doesn't respect HTML element indentation
-	// 		'markdownlint/md007': 'off',
-	// 		'no-autofix/markdownlint/md007': 'off',
-	// 	},
-	// },
+	defineConfiguration('json'),
+	defineConfiguration('markdown', {
+		overrides: {
+			// Doesn't respect HTML element indentation
+			// TODO: Maybe so automation to disable only the base rule
+			'markdownlint/md007': 'off',
+			'no-autofix/markdownlint/md007': 'off',
+			'markdownlint/md036': 'off',
+			// Doesn't take into account blockquotes with headlines
+			'markdownlint/md028': 'off',
+		},
+	}),
+	defineConfiguration('yaml'),
+	defineConfiguration('html'),
+	defineConfiguration('vitest'),
+	defineConfiguration('typescript'),
+	defineConfiguration('vue'),
+	defineConfiguration('astro'),
+	{
+		files: ['**/cli/**'],
+		rules: {
+			// Used intentionally for a better visual output
+			'n/no-process-exit': 'off',
+			// It's ok in CLIs
+			'no-await-in-loop': 'off',
+		},
+	},
 ]);
