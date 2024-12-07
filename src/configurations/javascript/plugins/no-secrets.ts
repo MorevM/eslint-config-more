1399298a786323c13960a3f740ebab6530eb281e
MorevM <max.seainside@gmail.com>
2024-12-07 19:47:12 +0300

    feat(javascript): Set up `no-secrets/no-secrets` rule properly

--- src/configurations/javascript/plugins/no-secrets.ts
+++ src/configurations/javascript/plugins/no-secrets.ts
@@ -1,26 +1,21 @@
 import { pluginNoSecrets } from '#plugins';
 import { defineConfigurationPart } from '#utils';
 
+// https://github.com/nickdeis/eslint-plugin-no-secrets
 export default defineConfigurationPart({
 	plugins: {
 		'no-secrets': pluginNoSecrets,
 	},
 	rules: {
-		/**
-		 * Seems it doesn't work if `additionalDelimiters` isn't an empty array.
-		 * Now i'm too busy to investigate into it, but anyway rule reports
-		 * well-known secrets like AWS fine
-		 *
-		 * TODO: [2025-05-11] Review
-		 */
-		'no-secrets/no-secrets': ['error', {
-			tolerance: 99,
+		'no-secrets/no-secrets': ['warn', {
+			tolerance: 5,
 			additionalRegexes: {},
 			ignoreContent: '^ABCD',
 			ignoreModules: true,
-			ignoreIdentifiers: [],
+			ignoreIdentifiers: ['BASE64_CHARS'],
 			ignoreCase: false,
-			additionalDelimiters: [],
+			additionalDelimiters: ['(?=[A-Z][a-z])'],
 		}],
+		'no-secrets/no-pattern-match': 'off',
 	},
 });
